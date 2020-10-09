616a9dcf902b56af7446be6d1b69dcf7a3241c73
Vipul Sharma <vipulsnp@gmail.com>
2020-10-09 14:14:29 +0530

    Updated SideDrawer, Made header sticky on top

--- src/Components/SideDrawer/SideDrawer.jsx
+++ src/Components/SideDrawer/SideDrawer.jsx
@@ -6,42 +6,55 @@ import styles from './sideDrawer.module.scss';
 
 export default function SideDrawer({ toggleSideDrawer }) {
   return (
-    <div className={styles.container}>
-      <span className={styles['button-div']}>
-        <button
-          type="button"
-          className={styles['close-button']}
-          onClick={toggleSideDrawer}>
-          <img src="/icons/hamburger.svg" alt="Menu" />
-        </button>
-      </span>
-      <a href="/" onClick={toggleSideDrawer}>
-        HOME
-      </a>
-      <a href="#about" onClick={toggleSideDrawer}>
-        ABOUT US
-      </a>
-      <a href="/" onClick={toggleSideDrawer}>
-        USER PROFILE
-      </a>
-      <a href="/feed" onClick={toggleSideDrawer}>
-        FEED
-      </a>
-      <a href="/" onClick={toggleSideDrawer}>
-        FAQ
-      </a>
-      <a href="/" onClick={toggleSideDrawer}>
-        DRAFTS
-      </a>
-      <a href="/" onClick={toggleSideDrawer}>
-        NOTIFICATIONS
-      </a>
-      <a href="/" onClick={toggleSideDrawer}>
-        DONATE
-      </a>
-      <a href="/" onClick={toggleSideDrawer}>
-        START WRITING
-      </a>
+    <div className={styles['outer-container']}>
+      <div className={styles.container}>
+        <span className={styles['button-div']}>
+          <button
+            type="button"
+            className={styles['close-button']}
+            onClick={toggleSideDrawer}>
+            <img src="/icons/hamburger.svg" alt="Menu" />
+          </button>
+        </span>
+        <a href="/" onClick={toggleSideDrawer}>
+          HOME
+        </a>
+
+        <a href="/" onClick={toggleSideDrawer}>
+          NOTIFICATIONS
+        </a>
+
+        <a href="/" onClick={toggleSideDrawer}>
+          USER PROFILE
+        </a>
+
+        <a href="/feed" onClick={toggleSideDrawer}>
+          FEED
+        </a>
+
+        <a href="/" onClick={toggleSideDrawer}>
+          START WRITING
+        </a>
+
+        <a href="/" onClick={toggleSideDrawer}>
+          DRAFTS
+        </a>
+
+        <a
+          href="/"
+          onClick={toggleSideDrawer}
+          style={{ color: '#4a2343', fontWeight: '600' }}>
+          DONATE
+        </a>
+
+        <a href="/" onClick={toggleSideDrawer}>
+          FAQ
+        </a>
+
+        <a href="#about" onClick={toggleSideDrawer}>
+          ABOUT US
+        </a>
+      </div>
     </div>
   );
 }
