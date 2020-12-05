be52cd23370823f3936e951eb1b56ff9dbf77a90
Vipul Sharma <vipulsnp@gmail.com>
2020-12-05 14:15:12 +0530

    Changed Login & SignUp methods

--- src/services/auth.js
+++ src/services/auth.js
@@ -6,13 +6,14 @@ const baseURL = `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/auth`;
   /* =================================== SIGN UP  =================================== */
 }
 
-export const signUp = async (name, email, password) => {
+export const signUp = async (name, email, password, username) => {
   return new Promise(async (resolve, reject) => {
     try {
       const data = {
         email,
         name,
-        password
+        password,
+        username
       };
       const res = await axios.post(`${baseURL}/signup`, data);
       localStorage.setItem('fbToken',res && res.data && res.data.token && res.data.token);
@@ -31,7 +32,7 @@ export const login = async (email, password) => {
   return new Promise(async (resolve, reject) => {
     try {
       const data = {
-        email,
+        "usernameOrEmail":email,
         password
       };
       const res = await axios.post(`${baseURL}/login`, data);
